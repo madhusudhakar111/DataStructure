e76fecec18cd27d551eb83ea6fc123ce247b4913
madhusudhakar111 <madhusudhakar.java@gmail.com>
2019-02-08 18:23:58 +0530

    Linked List Implementation

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -6,6 +6,8 @@ public class LinkedList {
 	Node tail;
 	int length;
 
+	// 3. Linked List Insertion
+	
 	public void add(int data) {
 		Node newNode = new Node(data);
 
@@ -28,13 +30,7 @@ public class LinkedList {
 		}
 	}
 
-	public void removeFirst() {
-		if (head.next != null) {
-			head = head.next;
-		}
-
-	}
-
+	
 	public void removeLast() {
 
 		Node prev = null;
@@ -48,6 +44,127 @@ public class LinkedList {
 		prev.next = null;
 
 	}
+
+	// 4. Linked List Deletion (Deleting a given key)
+	public void removeNode(int data) {
+
+		Node prev = null;
+		Node curNode = head;
+
+		while (curNode.data != data) {
+			prev = curNode;
+			curNode = curNode.next;
+		}
+
+		prev.next = curNode.next;
+
+	}
+	//5. Linked List Deletion (Deleting a key at given position)
+	public void removePosition(int position) {
+
+		Node prev = null;
+		Node curNode = head;
+		int counnter = 1;
+
+		while (counnter != position) {
+			prev = curNode;
+			curNode = curNode.next;
+			counnter++;
+		}
+
+		prev.next = curNode.next;
+	}
+	
+	// 6. Write a function to delete a Linked List
+	
+	public void removeFirst() {
+		if (head.next != null) {
+			head = head.next;
+		}
+
+	}
+
+	// 7.Find Length of a Linked List 
+
+	public int getCount() {
+		int counter = 0;
+		Node curNode = head;
+		while (curNode != null) {
+			curNode = curNode.next;
+			counter++;
+		}
+
+		return counter;
+	}
+
+	// 9. Write a function to get Nth node in a Linked List
+	public int findNthNode(int index) {
+
+		int counter = 1;
+		Node curNode = head;
+
+		while (counter != index) {
+			curNode = curNode.next;
+			counter++;
+		}
+
+		return curNode.data;
+	}
+
+	// 10. Nth node from the end of a Linked List
+	public int nthNodefromEnd(int position) {
+		int index = (length - position) + 1;
+		return findNthNode(index);
+	}
+	
+	// 11. Print the middle of a given linked list
+	public int middleElement() {
+		
+		// case 1 : 
+		
+		/**			
+			int count = getCount();	
+			int index = count/2;
+			int data = findNthNode(index);
+			return data;
+			
+			*/
+		
+		// case 2 : 
+				
+		Node slow_ptr = head;
+		Node fast_ptr = head;
+		
+		while(fast_ptr!=null && fast_ptr.next !=null) {
+			slow_ptr = slow_ptr.next;
+			fast_ptr = fast_ptr.next.next;
+			
+		}
+		
+		return slow_ptr.data;
+		
+	}
+	
+	
+	//12. Write a function that counts the number of times a given int occurs in a Linked List
+	public int countGivenInt(int data) {
+		
+		Node curNode = head;
+		int counter = 0;
+		
+		while(curNode !=null) {
+			if(curNode.data == data) {
+				counter++;
+			}
+			curNode = curNode.next;
+		}
+		
+		return counter;
+	}
+	
+	
+	
+	
 
 }
 
--- src/linkedList/Test.java
+++ src/linkedList/Test.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import java.util.HashMap;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -10,15 +12,55 @@ public class Test {
 		lst.add(3);
 		lst.add(4);
 		lst.add(5);
+		lst.add(1);
 
 		lst.print();
-		System.out.println(" Remove First Element");
+	
+		/**	System.out.println(" Remove First Element");
 		lst.removeFirst();
 		lst.print();
 		System.out.println(" Remove Last Element");
 		lst.removeLast();
 		lst.print();
+		*/
+		
+		/**
+		System.out.println(" Remove Node");
+		lst.removeNode(3);
+		lst.print();
+		*/
+		
+		
+	   /**System.out.println(" Remove Position");
+		lst.removePosition(4);
+		lst.print();
+		*/
+		
+		/**
+		 * System.out.println(" Get Count"); 
+		 * System.out.println(lst.getCount());
+		 */
+				
+		 /** System.out.println(" Find Nth node "); 
+		  System.out.println(lst.findNthNode(4));
+		   
+		*/
+		
+		
+		/**
+		 * System.out.println(" Find Nth node from end of Linked List ");
+		 * System.out.println(lst.nthNodefromEnd(2));
+		 */
+		
 
+		
+		  System.out.println(" Find count for given elements  Linked List ");
+		  System.out.println(lst.countGivenInt(1));
+		  
+		  
+		 
+		
+		
 	}
 
 }
